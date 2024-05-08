58ba1ae218e4724a5e5d9f355146b96a013277b2
Matija Cabadaj <matija.cabadaj@study.thws.de>
2024-05-08 22:28:09 +0200

    Centered splash screen

--- lib/presentation/pages/splash_page.dart
+++ lib/presentation/pages/splash_page.dart
@@ -12,6 +12,8 @@ class SplashPage extends StatelessWidget {
     return const Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             CircularProgressIndicator(),
             Text('Loading...'),
