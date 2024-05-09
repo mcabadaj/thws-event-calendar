ec350f7421b2e1adcbc43deb2cf1f3bbc330f821
Matija Cabadaj <matija.cabadaj@study.thws.de>
2024-05-09 16:42:10 +0200

    Removed System theme plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:system_theme/system_theme.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemTheme.fallbackColor = Colors.orange.shade600;
-  await SystemTheme.accentColor.load();
   runApp(const MainApp());
 }
 
